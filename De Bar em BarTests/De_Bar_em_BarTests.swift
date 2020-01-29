c47e3b9c3ae0d2cb320cc2967d50b02c1d918d97
Jonathan <jonathan@Jonathans-iMac.local>
2020-01-29 08:18:44 -0300

    Initial Commit

---
+++ De Bar em BarTests/De_Bar_em_BarTests.swift
@@ -0,0 +1,34 @@
+//
+//  De_Bar_em_BarTests.swift
+//  De Bar em BarTests
+//
+//  Created by Jonathan on 29/01/20.
+//  Copyright © 2020 hbsis. All rights reserved.
+//
+
+import XCTest
+@testable import De_Bar_em_Bar
+
+class De_Bar_em_BarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
